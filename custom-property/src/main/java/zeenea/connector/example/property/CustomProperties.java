de0c385f62886e1a062b6d844a56c702bbbd353e
Jérôme Mainaud <jmainaud@zeenea.com>
2024-11-22 14:18:23 +0100

    Add custom property documentation.

--- custom-property/src/main/java/zeenea/connector/example/property/CustomProperties.java
+++ custom-property/src/main/java/zeenea/connector/example/property/CustomProperties.java
@@ -5,13 +5,46 @@ import java.util.stream.Collectors;
 import zeenea.connector.exception.InvalidConfigurationException;
 import zeenea.connector.property.PropertyDefinition;
 
+/** A list of custom properties. */
 public class CustomProperties {
   private final List<CustomProperty> properties;
 
+  /**
+   * Create a new {@code CustomProperties} from a custom property list.
+   *
+   * @param properties A list of custom properties.
+   */
   public CustomProperties(List<CustomProperty> properties) {
     this.properties = List.copyOf(properties);
   }
 
+  /**
+   * Parse the definition of a custom property list.
+   *
+   * <p>The text should contain a list of property definitions each in form:
+   *
+   * <pre>
+   *         type <em>code</em> ( from <em>attributeName</em> ) ?
+   *     </pre>
+   *
+   * <p>Where type is one of:
+   *
+   * <ul>
+   *   <li>"string",
+   *   <li>"long text" or just "text",
+   *   <li>"tag",
+   *   <li>"number",
+   *   <li>"url",
+   *   <li>"instant".
+   * </ul>
+   *
+   * <p><em>code</em> and <em>name</em> value can be either quoted by single quote ("'") or raw
+   * name. A raw name starts with a letter or the underscore ("_") character and is followed by
+   * letters, digits, underscore ("_") or dash ("-").
+   *
+   * @param definitions The custom property list definition.
+   * @return A new {@code CustomProperties}.
+   */
   public static CustomProperties parse(String definitions) {
     if (definitions == null || definitions.isEmpty()) return new CustomProperties(List.of());
     try {
--- custom-property/src/main/java/zeenea/connector/example/property/CustomProperty.java
+++ custom-property/src/main/java/zeenea/connector/example/property/CustomProperty.java
@@ -1,5 +1,7 @@
 package zeenea.connector.example.property;
 
+import static java.util.Objects.requireNonNull;
+
 import java.util.Objects;
 import java.util.StringJoiner;
 import zeenea.connector.property.InstantPropertyDefinition;
@@ -11,15 +13,28 @@ import zeenea.connector.property.StringPropertyDefinition;
 import zeenea.connector.property.TagPropertyDefinition;
 import zeenea.connector.property.UrlPropertyDefinition;
 
+/**
+ * Define a custom property.
+ *
+ * <p>It consists of a Zeenea {@code PropertyDefinition} and an attribute name.
+ */
 public final class CustomProperty {
   private final PropertyDefinition definition;
   private final String attributeName;
 
-  public CustomProperty(PropertyDefinition definition, String attributeName) {
+  private CustomProperty(PropertyDefinition definition, String attributeName) {
     this.definition = definition;
     this.attributeName = attributeName;
   }
 
+  /**
+   * Create a new custom property.
+   *
+   * @param type Property type.
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty of(PropertyType type, String code, String attributeName) {
     switch (type) {
       case STRING:
@@ -39,42 +54,102 @@ public final class CustomProperty {
     }
   }
 
+  /**
+   * Create a new string custom property.
+   *
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty string(String code, String attributeName) {
-    return new CustomProperty(new StringPropertyDefinition(code), attributeName);
+    return new CustomProperty(
+        new StringPropertyDefinition(requireNonNull(code)), requireNonNull(attributeName));
   }
 
+  /**
+   * Create a new long text custom property.
+   *
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty longText(String code, String attributeName) {
-    return new CustomProperty(new LongTextPropertyDefinition(code), attributeName);
+    return new CustomProperty(
+        new LongTextPropertyDefinition(requireNonNull(code)), requireNonNull(attributeName));
   }
 
+  /**
+   * Create a new instant custom property.
+   *
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty instant(String code, String attributeName) {
-    return new CustomProperty(new InstantPropertyDefinition(code), attributeName);
+    return new CustomProperty(
+        new InstantPropertyDefinition(requireNonNull(code)), requireNonNull(attributeName));
   }
 
+  /**
+   * Create a new number custom property.
+   *
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty number(String code, String attributeName) {
-    return new CustomProperty(new NumberPropertyDefinition(code), attributeName);
+    return new CustomProperty(
+        new NumberPropertyDefinition(requireNonNull(code)), requireNonNull(attributeName));
   }
 
+  /**
+   * Create a new tag custom property.
+   *
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty tag(String code, String attributeName) {
-    return new CustomProperty(new TagPropertyDefinition(code), attributeName);
+    return new CustomProperty(
+        new TagPropertyDefinition(requireNonNull(code)), requireNonNull(attributeName));
   }
 
+  /**
+   * Create a new url custom property.
+   *
+   * @param code Property code.
+   * @param attributeName JSON attribute name.
+   * @return The new custom property.
+   */
   public static CustomProperty url(String code, String attributeName) {
-    return new CustomProperty(new UrlPropertyDefinition(code), attributeName);
+    return new CustomProperty(
+        new UrlPropertyDefinition(requireNonNull(code)), requireNonNull(attributeName));
   }
 
+  /**
+   * @return The property definition code.
+   */
   public String getCode() {
     return definition.getCode();
   }
 
+  /**
+   * @return The property definition type.
+   */
   public PropertyType getType() {
     return definition.getType();
   }
 
+  /**
+   * @return The property definition.
+   */
   public PropertyDefinition getDefinition() {
     return definition;
   }
 
+  /**
+   * @return The property attribute name.
+   */
   public String getAttributeName() {
     return attributeName;
   }

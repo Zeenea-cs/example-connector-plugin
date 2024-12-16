8f64f171de4ccfcf841340ad1c4d1a3a0c0cbc30
Jérôme Mainaud <jmainaud@zeenea.com>
2024-12-16 11:35:53 +0100

    Add CC0 licence on files.

--- filter/src/main/java/zeenea/connector/example/filter/Filter.java
+++ filter/src/main/java/zeenea/connector/example/filter/Filter.java
@@ -1,3 +1,9 @@
+/*
+ * This work is marked with CC0 1.0 Universal.
+ * To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
+ *
+ */
+
 package zeenea.connector.example.filter;
 
 import java.util.HashSet;
